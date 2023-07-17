1c5046f170aee8823daee3a6337e0c1f3ae6d17f
pawan1705 <pawan.sangare05@gmail.com>
2023-07-17 08:10:20 +0530

    thik

--- src/MainComponent/Main.js
+++ src/MainComponent/Main.js
@@ -9,8 +9,8 @@ function Main() {
   <div className="span5">
     <h1>Pawan Sangare</h1>
     <h3>Web &amp; App Developer</h3>
-    <p> Hello I am Pawan Kumar Sangare Web,App  and  Software Developer from Indore . If yopu have something to be done just call my name and I will be there. :-) </p>
-    <a href="https://www.instagram.com/pawan_sangare_/" className="hire-me"><i className="icon-paper-plane"></i> Hire Me </a> </div>
+    <p> Hello I am Pawan Kumar Sangare Web,App  and  Software Developer from Indore . If you have something to be done just contact me and I will be there. :-) </p>
+    <a href="https://www.instagram.com/pawan_sangare_/" className="hire-me"><i className="icon-paper-plane"></i> Contact Me </a> </div>
 </div>
     </div>
   );
